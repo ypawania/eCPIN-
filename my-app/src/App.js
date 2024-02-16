fe2a414186bd59bf18a6304a78f9dd9289f408e5
Yohance <ypawania@gmail.com>
2024-02-15 20:53:58 -0500

    Made Routes Work
    added linking routes to navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Contact from './Pages/Contact';
 import Navbar from './Components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Routes>
+       
+        
+        <Route path="/Home" element={ <Home /> } />
+        <Route path = "/About" element={ <About />} />
+        <Route path="/Contact" element={<Contact />} />
+       
+
+      
+    </Routes>
+      
     </div>
   );
 }
--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,11 +1,17 @@
+import { Outlet, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NavLink } from 'react-router-dom'
+
+
 const pages = ['Home','Browse', 'Search', 'About','Contact'];
 
 
 function NavLinkPage({title}){
-    const textHash = '#';
+    const textHash = '/';
     return (
         <div className='listItem'>
-            <a href={textHash.concat(title)}>{title}</a>
+            <NavLink to={textHash.concat(title)}>{title}</NavLink>
+            <Outlet/>
         </div>
     )
 }
@@ -14,6 +20,7 @@ export default function Navbar(){
     const pagesItems = pages.map(page => <NavLinkPage title={page}/>)
     return (
         <div className='topnav'>
+            <NavLink to={"/"}>Home</NavLink>
             {pagesItems}
         </div>
     )
